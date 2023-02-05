09fc0365aed294f75bf0468b73f6c54dba438be7
ByeonSeongUk <seonguk092@gmail.com>
2023-02-05 10:37:38 +0900

    feat(seonguk): initial setting-7

--- src/components/BoardList.tsx
+++ src/components/BoardList.tsx
@@ -1,9 +1,10 @@
-import {Col, Container, Pagination, Row} from "react-bootstrap";
+import {Button, Container, Pagination, Row} from "react-bootstrap";
 import BoardBlock from "./BoardBlock";
 import {Board} from "../Redux/Slices/board";
 import {useDispatch, useSelector} from "react-redux";
 import {ReducerType} from "../Redux/rootReducer";
 import {useState} from "react";
+import axios from "axios";
 
 const BoardList = () => {
 
@@ -33,6 +34,7 @@ const BoardList = () => {
         <>
             <Container fluid={"sm"}>
                 <h2 style={{borderBottom:'1px solid #D2D4D9', paddingBlock: '5px', textAlign: 'left'}}>게시판 화면</h2>
+                <Button onClick={getBoardList}>호출</Button>
 
                 {/* 게시물 내용 */}
                 {
@@ -61,5 +63,12 @@ const BoardList = () => {
         </>
     )
 }
-
+const getBoardList = async() => {
+    // Generic 을 통해 응답 데이터의 타입을 설정 할 수 있습니다.
+    const response = await axios.get<Board>(
+        `http://localhost:8080/api/board/selectlist`
+    );
+    console.log(response.data);
+    return response.data; // 데이터 값을 바로 반환하도록 처리
+}
 export default BoardList;
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,6 +1,8 @@
 import Github from "./Github";
 import {Button, Container, Form} from "react-bootstrap";
 import React from "react";
+import axios from "axios/index";
+import {GithubProfile} from "../types/GithubProfile";
 
 const Login = () => {
 
@@ -21,9 +23,11 @@ const Login = () => {
                         <Form.Label>PW</Form.Label>
                         <Form.Control type="password" placeholder="Password" />
                     </Form.Group>
+
                     <Form.Group className="mb-3" controlId="formBasicCheckbox">
                         <Form.Check type="checkbox" label="Check me out" />
                     </Form.Group>
+
                     <Button variant="primary" type="submit">
                         Submit
                     </Button>
@@ -32,5 +36,13 @@ const Login = () => {
             </Container>
         </>
     )
+}
+const loginTest = async() => {
+    // Generic 을 통해 응답 데이터의 타입을 설정 할 수 있습니다.
+    const response = await axios.post(
+        `http://localhost:8080/api/setboard`
+    );
+    console.log(response.data);
+    return response.data; // 데이터 값을 바로 반환하도록 처리
 }
 export default Login;
